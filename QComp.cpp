5b11ab83d61d1ebe107c953daa192bdf4f9889ff
Nathaniel Gerlek <nathaniel.gerlek@gmail.com>
2022-12-03 17:04:22 -0800

    Outlined QComp class, added apply() & printState()

--- QComp.cpp
+++ QComp.cpp
@@ -1,16 +1,31 @@
 #ifndef QCOMP_CPP_12_2_2022_21_55
 #define QCOMP_CPP_12_2_2022_21_55
 
+#include <iostream>
 #include "QComp.h"
 
 QComp::QComp()
 {
-    //
+    this->state = new State();
 }
 
 QComp::~QComp()
 {
-    //
+    delete this->state;
+}
+
+void QComp::apply(Gate *gate)
+{
+    gate->applyGate(this->state);
+}
+
+bool QComp::measure(int q) {
+    // TODO
+}
+
+void QComp::printState()
+{
+    std::cout << state->toString();
 }
 
 #endif
--- QComp.h
+++ QComp.h
@@ -10,12 +10,9 @@ public:
     QComp();
     ~QComp();
     void clear();
-    void applyCNot(int q1, int q2);
-    void applyH(int q);
-    void applyX(int q);
-    void applyY(int q);
-    void applyZ(int q);
+    void apply(Gate *gate);
     bool measure(int q);
+    void printState();
 
 private:
     State *state;
